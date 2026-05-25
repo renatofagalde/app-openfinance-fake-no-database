23f8eaf8a49fa7be9d1361af4450a235ead6614b
Renato Fagalde <renato@likwi.com.br>
2026-05-25 20:51:18 -0300

    Move route definitions and Gin setup to `Source/internal` directory

--- Source/internal/adapter/input/routes/routes.go
+++ Source/internal/adapter/input/routes/routes.go
@@ -1,19 +1,12 @@
 package routes
 
 import (
+	"bootstrap/internal/adapter/input/controller"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/renatofagalde/app-openfinance-fake-no-database/internal/adapter/input/controller"
 )
 
-// Setup registra apenas o que e fixo no Gin:
-//   - healthz
-//   - endpoints administrativos sob /_admin/...
-//
-// Todas as demais requisicoes caem no MockController via gin.NoRoute,
-// permitindo que rotas mockadas sejam adicionadas em runtime sem restart.
 func Setup(r *gin.Engine, mockCtrl *controller.MockController, adminCtrl *controller.AdminController) {
 	r.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
 
